c445bfb5e22fa5d11b5e53fe12e55f44bd68a043
Nalini Gujrati <gujrati.nalini@gmail.com>
2015-07-11 19:36:01 +0530

    Update questionnaire

--- js/wizard.js
+++ js/wizard.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
     /*  Activate the tooltips      */
     $('[rel="tooltip"]').tooltip();
     
-        
+
     $('#wizard').bootstrapWizard({
         'tabClass': 'nav nav-pills',
         'nextSelector': '.btn-next',
@@ -23,7 +23,22 @@ $(document).ready(function(){
            }
            navigation.find('li').css('width',$width + '%');
         },
-         onTabClick : function(tab, navigation, index){
+        onNext : function(tab, navigation, index){
+            var wizard = navigation.closest('.wizard-card');
+            var tabpaneId = $(navigation.find('li a')[index-1]).attr('href');
+            var tabPane = $(wizard).find(tabpaneId)
+            var hasErrors = $(tabPane).validator('validate').has('.has-error').length;
+            if (hasErrors > 0) {
+                $('html, body').animate({ scrollTop: $(".has-error:first :input").offset().top - 100 }, 600);
+                $(".has-error:first :input").focus();
+                return false;
+            } else {
+                return true;
+            }
+/*            // Disable the posibility to click on tabs
+            return false;
+*/        },        
+        onTabClick : function(tab, navigation, index){
             // Disable the posibility to click on tabs
             return false;
         }, 
@@ -57,30 +72,44 @@ $(document).ready(function(){
         radio_group = $(this).closest('.radio-group');
         radio_group.find('[data-toggle="wizard-radio"]').removeClass('active');
         $(this).addClass('active');
-        $(radio_group).find('[type="checkbox"]').removeAttr('checked');
-        $(this).find('[type="checkbox"]').attr('checked','true');
+        $(radio_group).find('[type="radio"]').prop('checked', false);
+        $(this).find('[type="radio"]').prop('checked','true');
     });
     
     $('[data-toggle="wizard-checkbox"]').click(function(event){
         $(this).toggleClass('active');
         checkbox = $(this).find('[type="checkbox"]')
-        checkbox.attr('checked',!checkbox.attr("checked"));
+        checkbox.prop('checked',!checkbox.prop("checked"));
     });
     
     $('[data-toggle="set-checked"]').click(function(event){
         checkbox = $(this).find('[type="checkbox"]')
-        checkbox.attr('checked',!checkbox.attr("checked"));
+        checkbox.prop('checked',!checkbox.prop("checked"));
     });
 
     $height = $(document).height();
     $('.set-full-height').css('height',$height);
-    
-    //functions for demo purpose
+
+
     $(".btn-finish").button().click(function(){
-        $('form#questionnaire').serializeJSON();
+        waitingDialog.show("Please wait...");
+        var data = JSON.stringify($('form#questionnaire').serializeJSON({useIntKeysAsArrayIndex: true}));
+        $.ajax({
+            url: "https://docs.google.com/forms/d/1F41Hvu-lKT996Fj_UF78GBL0Xax026gHnR4Feydjic4/formResponse",
+            data: { "entry.1378759943": data
+             },
+            type: "POST",
+            dataType: "json",
+        }).always(function () {
+            waitingDialog.hide()
+            window.location.replace("connect_thankyou.html");
+        });
     });
     
 });
+
+    //functions for demo purpose
+
 
 
  //Function to show image before upload
